bd5bd2e4ca5f4cad89baad2b0b5ddfc590cfb1c3
Marius Jabami <138234634+mariusjabami@users.noreply.github.com>
2026-02-08 18:47:19 +0000

    Atualizar o parser.c

--- src/parser.c
+++ src/parser.c
@@ -1,41 +1,53 @@
 #include <stdio.h>
 #include "../includes/parser.h"
 
-void print_f(int len, int num, char (*text)[len]){
+/*
+ * Prints a fixed-size array of strings.
+ *
+ * Parameters:
+ *  len  - length of each string
+ *  num  - number of strings to print
+ *  text - 2D char array containing the strings
+ *
+ * Behavior:
+ *  - Iterates through the array
+ *  - Prints each string to stdout
+ */
+void print_f(int len, int num, char (*text)[len]) {
 
-
-	for (int i = 0; i < num; i++) {
-	
-		printf("%s", text[i]);
-	}
-
+    for (int i = 0; i < num; i++) {
+        printf("%s", text[i]);
+    }
 }
 
-void print_fdyn(int len, int num, char **dir){
+/*
+ * Reads a text file line by line into a fixed buffer.
+ *
+ * Parameters:
+ *  file - path to the file to be read
+ *  text - destination buffer (max 100 lines, 100 chars each)
+ *
+ * Returns:
+ *  - Number of lines read on success
+ *  - -1 if file could not be opened
+ *
+ * Behavior:
+ *  - Opens file in read mode
+ *  - Reads up to 100 lines
+ *  - Stops on EOF or buffer limit
+ */
+int read_file(char *file, char (*text)[100]) {
 
-	for (int i = 0; i < num; i++) {
-	
-		printf("\n%s", dir[i]);
-	}
+    int i = 0;
+    FILE *f = fopen(file, "r");
 
+    if (!f)
+        return -1;
+
+    while (i < 100 && fgets(*(text + i), 100, f) != NULL) {
+        i++;
+    }
+
+    fclose(f);
+    return i;
 }
-
-int read_file(char *file, char (*text)[100]){
-
-	int i = 0;
-
-	FILE *f = fopen(file, "r");
-
-	if (!f) return -1;
-
-	while (i < 100 && fgets(*(text + i), 100, f) != NULL) {
-		i++;
-	}
-
-	fclose(f);
-
-	return i;
-
-}
-
-
